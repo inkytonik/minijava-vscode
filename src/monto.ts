76af41782959912f0d2d024a574957985e450c8e
Tony Sloane <tony.sloane@me.com>
2020-03-25 13:46:22 +1100

    Add MPL notice to src files

--- src/monto.ts
+++ src/monto.ts
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import { commands, ExtensionContext, EventEmitter, Range, Selection, TextDocumentContentProvider, TextEditor, TextEditorRevealType, TextEditorSelectionChangeEvent, Uri, ViewColumn, workspace, window } from 'vscode';
 import { NotificationType } from 'vscode-jsonrpc';
 import { LanguageClient, DidChangeConfigurationNotification } from 'vscode-languageclient';
